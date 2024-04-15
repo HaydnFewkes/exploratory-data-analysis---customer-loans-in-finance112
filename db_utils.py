1d689c75c0a89af76f217993b522b37502e170b1
Haydn Fewkes <haydnsfewkes@outlook.com>
2024-04-15 14:58:35 +0100

    Updated method

--- db_utils.py
+++ db_utils.py
@@ -1,12 +1,19 @@
 import yaml
+from sqlalchemy import create_engine
+import pandas as pd
 
 with open('credentials.yaml', 'r') as file:
     creds = yaml.safe_load(file)
 
+print(creds)
 
 class RDSDatabaseConnector:
     def __init__(self, creds):
-        self.creds = creds
-
-    def SQLAlchemy():
-        pass
+        DATABASE_TYPE = 'postgresql'
+        DBAPI = 'psycopg2'
+        HOST = creds['RDS_HOST']
+        USER = creds['RDS_USER']
+        PASSWORD = creds['RDS_PASSWORD']
+        DATABASE = creds['RDS_DATABASE']
+        PORT = creds['RDS_PORT']
+        engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
