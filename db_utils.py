51ab2d769e1d54bf44a28f0556d3cf76941b4ffd
Haydn Fewkes <haydnsfewkes@outlook.com>
2024-05-08 15:35:55 +0100

    added dataframeinfo class

--- db_utils.py
+++ db_utils.py
@@ -3,8 +3,10 @@ from sqlalchemy import create_engine, inspect
 import pandas as pd
 
 # Opens the database credentails, and saves them
-with open('credentials.yaml', 'r') as file:
-    creds = yaml.safe_load(file)
+def GetCreds():
+    with open('credentials.yaml', 'r') as file:
+        creds = yaml.safe_load(file)
+        return creds
 
 
 class RDSDatabaseConnector:
@@ -48,21 +50,50 @@ class DataTransform:
     def __init__(self, database):
         self.df = database
 
-    def convert_to_datetime(self, column_name):
-        self.df[column_name] = pd.to_datetime(self.df[column_name])
+    def convert_to_datetime(self, column_names):
+        for column in column_names:
+            self.df[column] = pd.to_datetime(self.df[column])
 
     def convert_to_int(self):
         self.df['term'] = self.df['term'].str.replace('months','')
         self.df['term'] = self.df['term'].astype(float)
         self.df.rename(columns={'term':'term_in_mnths'})
-        print(self.df.dtypes)
 
+class DataFrameInfo:
+    def __init__(self, database):
+        self.df = database
+
+    def check_datatypes(self):
+        return self.df.dtypes
+
+    def get_mean(self):
+        return self.df.mean(skipna=True, numeric_only=True)
     
+    def get_median(self):
+        return self.df.median(axis=0, numeric_only=True)
+    
+    def get_standard_deviation(self):
+        return self.df.std(skipna=True,numeric_only=True)
+    
+    def get_distinct_values(self):
+        return self.df.nunique()
+
+    def get_shape(self):
+        return self.df.shape
+
+    def get_null_values(self):
+        return self.df.isna().sum()
+
+creds = GetCreds()
 a = RDSDatabaseConnector(creds)
 a.RDSConnection()
 a.RDSExtract()
 #a.RDSSaveToCSV()
 
 b = DataTransform(a.loan_payments)
-#b.convert_to_datetime('issue_date')
+b.convert_to_datetime(['issue_date','last_payment_date','next_payment_date','earliest_credit_line'])
 b.convert_to_int()
+
+c = DataFrameInfo(b.df)
+print(c.get_null_values())
+#USE IPYNB!!!!!
